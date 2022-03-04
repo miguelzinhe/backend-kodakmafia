42e3e5c3078a67d7f9c4c1fe07f01b45d6d48d04
Misoare <Miguel_Soares@gap.com>
2022-03-04 18:01:51 -0300

    app: add connection to mongo

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = require("./router");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const PORT = 8000;
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(router);
+
+app.listen(PORT, async () => {
+  console.log(`server up on port ${PORT}`);
+});
